87c261d7bf43e8ce9f5c4b4ae1092e1059dc21ef
Jino-Lee <90809030+Leetany@users.noreply.github.com>
2025-03-25 14:55:23 +0900

    2D횡스크롤 공부2

--- KatanaSide/Assets/Script/Player.cs
+++ KatanaSide/Assets/Script/Player.cs
@@ -1,11 +1,26 @@
-﻿using UnityEngine;
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("플레이어 속성")]
     public float speed = 5;
     public float jumpUp = 10;
+    public float power = 5;
     public Vector3 direction;
     public GameObject slash;
+
+
+    //그림자
+    public GameObject Shadow1;
+    List<GameObject> sh = new List<GameObject>();
+
+    //히트 이펙트
+    public GameObject hit_lazer;
+
+
 
     bool bJump = false;
     Animator pAnimator;
@@ -65,21 +80,38 @@ public class Player : MonoBehaviour
             //left
             sp.flipX = true;
             pAnimator.SetBool("Run", true);
+
+            for(int i = 0; i<sh.Count; i++)
+            {
+                sh[i].GetComponent<SpriteRenderer>().flipX = sp.flipX;
+            }
         }
         else if(direction.x > 0)
         {
             //right
             sp.flipX = false;
             pAnimator.SetBool("Run", true);
+
+            for (int i = 0; i < sh.Count; i++)
+            {
+                sh[i].GetComponent<SpriteRenderer>().flipX = sp.flipX;
+            }
         }
         else if(direction.x == 0)
         {
             pAnimator.SetBool("Run", false);
+
+            for(int i = 0; i< sh.Count; i++)
+            {
+                Destroy(sh[i]); //게임 오브젝트 지우기
+                sh.RemoveAt(i); //게임 오브젝트 관리하는 리스트 지우기
+            }
         }
 
         if(Input.GetMouseButtonDown(0)) //0번 왼쪽마우스
         {
             pAnimator.SetTrigger("Attack");
+            Instantiate(hit_lazer, transform.position, Quaternion.identity);
         }
 
     }
@@ -98,7 +130,29 @@ public class Player : MonoBehaviour
 
     public void AttSlash()
     {
-        //플레이어 오른쪽
-        Instantiate(slash, transform.position, Quaternion.identity);
+        //플레이어 오른쪽 
+        if(sp.flipX == false)
+        {
+            pRig2D.AddForce(Vector2.right * power, ForceMode2D.Impulse);
+            GameObject go = Instantiate(slash, transform.position, Quaternion.identity);
+            go.GetComponent<SpriteRenderer>().flipX = sp.flipX;
+        }
+        else
+        {
+            pRig2D.AddForce(Vector2.left * power, ForceMode2D.Impulse);
+            //왼쪽
+            GameObject go = Instantiate(slash, transform.position, Quaternion.identity);
+            go.GetComponent<SpriteRenderer>().flipX = sp.flipX;
+        }
+    }
+
+    public void RunShadow()
+    {
+        if(sh.Count<6)
+        {
+            GameObject go = Instantiate(Shadow1, transform.position, Quaternion.identity);
+            go.GetComponent<Shadow>().TwSpeed = 10 - sh.Count;
+            sh.Add(go);
+        }
     }
 }
